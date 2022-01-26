636067300182b311d902cb35ad9fa6cfa95835a7
kuzmychAndrew <89782153+kuzmychAndrew@users.noreply.github.com>
2022-01-26 13:00:00 +0200

    Initial Commit

---
+++ New_8_Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New_8_Ball
+//
+//  Created by Андрій Кузьмич on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
